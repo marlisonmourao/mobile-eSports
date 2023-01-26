6fe9dfc2a4cdc99160ac3dffb2e398d7f8ee8da5
marlisonmourao <bentesmourao@gmail.com>
2023-01-26 01:15:18 -0400

    fix gameCard

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -13,9 +13,11 @@ import { THEME } from "../../theme";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -30,18 +32,18 @@ export function GameCard({ data, ...rest }: Props) {
     >
       <ImageBackground 
         style={styles.cover} 
-        source={data.cover}
+        source={{ uri: data.bannerUrl }}
       >
         <LinearGradient 
           colors={THEME.COLORS.FOOTER} 
           style={styles.footer}
         >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
 
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
           </Text>
           
         </LinearGradient>
